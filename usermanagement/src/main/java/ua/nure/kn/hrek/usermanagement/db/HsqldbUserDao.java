3482e2f4ca9aa210de8f95da88c02217de3b0289
EgorGrek <gromer349@gmail.com>
2019-11-16 21:46:15 +0200

    implement methods delete, find, update

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/db/HsqldbUserDao.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/db/HsqldbUserDao.java
@@ -17,15 +17,18 @@ class HsqldbUserDao implements UserDao {
 	private ConnectionFactory connectionFactory;
 
 	private String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES(?, ?, ?);";
+	private String UPDATE_QUERY = "UPDATE users SET firstname = ?, lastname = ?, dateofbirth = ? WHERE id = ?";
+	private String DELETE_QUERY = "DELETE FROM users WHERE id = ?";
+	private String FIND_QUERY = "SELECT * FROM users WHERE id = ?";
 	private String SELECT_ALL_QUERY = "SELECT id, firstname, lastname, dateofbirth FROM users;";
-	
+
 	public HsqldbUserDao() {
 	}
 
 	public HsqldbUserDao(ConnectionFactory connectionFactory) {
 		this.connectionFactory = connectionFactory;
 	}
-	
+
 	public ConnectionFactory getConnectionFactory() {
 		return connectionFactory;
 	}
@@ -34,9 +37,7 @@ class HsqldbUserDao implements UserDao {
 		this.connectionFactory = connectionFactory;
 	}
 
-
 	public User create(User user) throws DatabaseException {
-
 		try {
 			Connection connection = connectionFactory.createConnection();
 			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
@@ -44,49 +45,92 @@ class HsqldbUserDao implements UserDao {
 			statement.setString(2, user.getLastName());
 			statement.setDate(3, new java.sql.Date(user.getDateOfBirth().getTime()));
 			int n = statement.executeUpdate();
-			if(n != 1) {
+			if (n != 1) {
 				throw new DatabaseException("Number of the inserrted rows: " + n);
 			}
 			CallableStatement callebleStatement = connection.prepareCall("call IDENTITY()");
 			ResultSet keys = callebleStatement.executeQuery();
-			if(keys.next()) {
+			if (keys.next()) {
 				user.setId(new Long(keys.getLong(1)));
 			}
 			keys.close();
 			callebleStatement.close();
 			statement.close();
 			connection.close();
-			return user;
 		} catch (DatabaseException e) {
 			throw e;
 		} catch (SQLException e) {
 			throw new DatabaseException(e);
 		}
+		return user;
 	}
 
-	public void update(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
+	public void update(User entity) throws DatabaseException {
 
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+
+			preparedStatement.setString(1, entity.getFirstName());
+			preparedStatement.setString(2, entity.getLastName());
+			preparedStatement.setDate(3, new Date(entity.getDateOfBirth().getTime()));
+			preparedStatement.setLong(4, entity.getId());
+
+			preparedStatement.close();
+			connection.close();
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
-	public void dalete(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
+	public void delete(User entity) throws DatabaseException {
 
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+
+			preparedStatement.setLong(1, entity.getId());
+
+			preparedStatement.close();
+			connection.close();
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
 	}
 
 	public User find(Long id) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+
+		User user = new User();
+
+		try {
+			Connection connection = connectionFactory.createConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+
+			preparedStatement.setLong(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				user.setId(resultSet.getLong(1));
+				user.setFirstName(resultSet.getString(2));
+				user.setLastName(resultSet.getString(3));
+				user.setDateOfBirth(resultSet.getDate(4));
+			}
+
+			preparedStatement.close();
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+		return user;
 	}
 
 	public Collection findAll() throws DatabaseException {
 		Collection result = new LinkedList();
-		
+
 		try {
 			Connection connection = connectionFactory.createConnection();
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY);
-			while(resultSet.next()) {
+			while (resultSet.next()) {
 				User user = new User();
 				user.setId(new Long(resultSet.getLong(1)));
 				user.setLastName(resultSet.getString(2));
@@ -96,7 +140,7 @@ class HsqldbUserDao implements UserDao {
 			}
 		} catch (DatabaseException e) {
 			throw e;
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DatabaseException(e);
 		}
 		return result;
