b1116149ef8fd63ee6cb5b86e8cc737f5de0d96d
EgorGrek <gromer349@gmail.com>
2019-11-24 12:21:12 +0200

    impl AddPanel

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/gui/AddPanel.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/gui/AddPanel.java
@@ -2,11 +2,14 @@ package ua.nure.kn.hrek.usermanagement.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AddPanel extends JPanel implements ActionListener {
 	
@@ -15,6 +18,9 @@ public class AddPanel extends JPanel implements ActionListener {
 	private JPanel fieldPanel;
 	private JButton okButton;
 	private JButton cancelButton;
+	private JTextField dayOfBirthField;
+	private JTextField lastNameField;
+	private JTextField firstNameField;
 	
 	public AddPanel(MainFrame mainFrame) {
 		this.parent = mainFrame;
@@ -49,9 +55,48 @@ public class AddPanel extends JPanel implements ActionListener {
 	}
 
 	private JPanel getFieldPanel() {
-		// TODO Auto-generated method stub
+		if(fieldPanel == null) {
+			fieldPanel = new JPanel();
+			fieldPanel.setLayout(new GridLayout(3, 2));
+			addLabeledField(fieldPanel, "Имя", getFirstNameField());
+			addLabeledField(fieldPanel, "Фамилия", getLastNameField());
+			addLabeledField(fieldPanel, "Дата рождения", getDayOfBirthField());
+		}
 		return fieldPanel;
 	}
+	private JTextField getDayOfBirthField() {
+		if(dayOfBirthField == null) {
+			dayOfBirthField = new JTextField();
+			dayOfBirthField.setName("datOfBirthField");
+		}
+		return dayOfBirthField;
+	}
+
+	private JTextField getLastNameField() {
+		if(lastNameField == null) {
+			lastNameField = new JTextField();
+			lastNameField.setName("lastNameField");
+		}
+		return lastNameField;
+	}
+
+	private JTextField getFirstNameField() {
+		if(firstNameField == null) {
+			firstNameField = new JTextField();
+			firstNameField.setName("firstNameField");
+		}
+		return firstNameField;
+	}
+	
+	private void addLabeledField(JPanel panel, String labelText, JTextField textField) {
+		JLabel label = new JLabel(labelText);
+		label.setLabelFor(textField);
+		panel.add(label);
+		panel.add(textField);
+	}
+
+
+
 	private JButton getOkButton() {
 		if (okButton == null){
 			okButton = new JButton();
