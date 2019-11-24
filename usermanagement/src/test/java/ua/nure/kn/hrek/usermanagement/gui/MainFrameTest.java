178a835061df6aa8359cb0b41ce038d6b7f1095e
EgorGrek <gromer349@gmail.com>
2019-11-24 09:49:09 +0200

    add testBrowseControls

--- usermanagement/src/test/java/ua/nure/kn/hrek/usermanagement/gui/MainFrameTest.java
+++ usermanagement/src/test/java/ua/nure/kn/hrek/usermanagement/gui/MainFrameTest.java
@@ -1,11 +1,19 @@
 package ua.nure.kn.hrek.usermanagement.gui;
 
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.finder.NamedComponentFinder;
 
 class MainFrameTest extends JFCTestCase {
 
@@ -26,9 +34,25 @@ class MainFrameTest extends JFCTestCase {
 		super.tearDown();
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	private Component find(Class componentClass, String name) {
+		NamedComponentFinder finder;
+		finder = new NamedComponentFinder(componentClass, name);
+		finder.setWait(0);
+		Component component = finder.find(mainFrame, 0);
+		assertNotNull("Could not find component '" + name + "'", component);
+		
+		return component;
 	}
+	@Test
+	public void testBrowseControls() {
+		find(JPanel.class, "browsePanel");
+		find(JTable.class, "userTable");
+		find(JButton.class, "addButton");
+		find(JButton.class, "editButton");
+		find(JButton.class, "deleteButton");
+		find(JButton.class, "detailsButton");
+	}
+	
 
 }
