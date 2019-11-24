ce3b57b5b0b493df13578ccf2e4fc576ea0b27c9
EgorGrek <gromer349@gmail.com>
2019-11-24 11:04:18 +0200

    impl method initialize

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/gui/MainFrame.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/gui/MainFrame.java
@@ -1,11 +1,52 @@
 package ua.nure.kn.hrek.usermanagement.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 
+	
+	private static final int FRAME_HEIGHT = 600;
+	private static final int FRAME_WIDTH = 800;
+	private JPanel contentPanel;
+	private Component browsePanel;
+
+	public MainFrame() {
+		super();
+		initialize();
+	}
+	
+	private void initialize() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		this.setTitle("Управление пользователями");
+		this.setContentPane(getContentPanel());
+	}
+
+	
+	private JPanel getContentPanel() {
+		if(contentPanel == null) {
+			contentPanel = new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+			
+		}
+		return contentPanel;
+	}
+	private Component getBrowsePanel() {
+		if(browsePanel == null) {
+			browsePanel = new BrowsePanel(this);
+		}
+		return browsePanel;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 
 	}
 
