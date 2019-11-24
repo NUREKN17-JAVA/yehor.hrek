46703ef1526950feda0835f723b3525a585e1835
EgorGrek <gromer349@gmail.com>
2019-11-24 11:03:05 +0200

    add BrowsePanel

---
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,11 @@
+package ua.nure.kn.hrek.usermanagement.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+
+	public BrowsePanel(MainFrame mainFrame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
