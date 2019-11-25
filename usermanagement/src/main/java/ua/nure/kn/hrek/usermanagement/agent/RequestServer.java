76ed4cb77acda1cbc29ea3e108c68618c1e38f51
EgorGrek <gromer349@gmail.com>
2019-11-25 12:48:19 +0200

    impl RequestServer

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/RequestServer.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/RequestServer.java
@@ -1,13 +1,35 @@
 package ua.nure.kn.hrek.usermanagement.agent;
 
+import java.util.Collection;
+
 import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class RequestServer extends CyclicBehaviour {
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
+		ACLMessage message = myAgent.receive();
+		if(message!= null) {
+			if(message.getPerformative() == ACLMessage.REQUEST) {
+				myAgent.send(createReply(message));
+			}else {
+				Collection users = parseMessage(message);
+				((SearchAgent)myAgent).showUsers(users);
+			}
+		}else {
+			block();
+		}
+	}
 
+	private Collection parseMessage(ACLMessage message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ACLMessage createReply(ACLMessage message) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
