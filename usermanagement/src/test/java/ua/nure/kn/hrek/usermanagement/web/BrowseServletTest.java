220ebc9a1dfc2db35b8a338dd8c7c3b06e42b94b
EgorGrek <gromer349@gmail.com>
2019-11-25 21:10:07 +0200

    add test for web ui

---
+++ usermanagement/src/test/java/ua/nure/kn/hrek/usermanagement/web/BrowseServletTest.java
@@ -0,0 +1,18 @@
+package ua.nure.kn.hrek.usermanagement.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BrowseServletTest extends MockServletTestCase {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
