413f1f8e8381f8879b3eb709226bb86edbcca7a2
EgorGrek <gromer349@gmail.com>
2019-11-25 12:37:16 +0200

    impl SearchAgent

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/SearchAgent.java
@@ -1,6 +1,10 @@
 package ua.nure.kn.hrek.usermanagement.agent;
 
+import java.util.Collection;
+
 import jade.core.Agent;
+import ua.nure.kn.hrek.usermanagement.db.DaoFactory;
+import ua.nure.kn.hrek.usermanagement.db.DatabaseException;
 
 public class SearchAgent extends Agent {
 
@@ -14,6 +18,23 @@ public class SearchAgent extends Agent {
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated.");
 		super.takeDown();
+	}
+	
+	public void search(String firstName, String lastName) throws SearchException{
+		try {
+			Collection users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			if(users.size() > 0) {
+				showUsers(users);
+			}else {
+				addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+			}
+		}catch(DatabaseException e){
+			throw new SearchException(e);
+		}
+	}
+	
+	private void showUsers(Collection users) {
+		//TODO отобразить найденных пользователей
 	}
 
 }
