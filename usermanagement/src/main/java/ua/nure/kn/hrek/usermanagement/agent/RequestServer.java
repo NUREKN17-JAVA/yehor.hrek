b3acdaf7848bb08234d082488ac06421242469c8
EgorGrek <gromer349@gmail.com>
2019-11-25 12:42:04 +0200

    add RequestServer

---
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/RequestServer.java
@@ -0,0 +1,13 @@
+package ua.nure.kn.hrek.usermanagement.agent;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class RequestServer extends CyclicBehaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
