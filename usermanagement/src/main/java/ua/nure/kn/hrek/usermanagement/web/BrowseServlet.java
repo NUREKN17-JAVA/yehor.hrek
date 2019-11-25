27d520e4ba615f26ed54d3ff67ff6dce512f43bd
EgorGrek <gromer349@gmail.com>
2019-11-25 21:47:39 +0200

    impl BrowseServlet

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
@@ -1,7 +1,37 @@
 package ua.nure.kn.hrek.usermanagement.web;
 
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ua.nure.kn.hrek.usermanagement.db.DaoFactory;
+import ua.nure.kn.hrek.usermanagement.db.DatabaseException;
 
 public class BrowseServlet extends HttpServlet {
 
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		browse(req, resp);
+		
+	}
+
+	private void browse(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		Collection users;
+		try {
+			users = DaoFactory.getInstance().getUserDao().findAll();
+			req.getSession().setAttribute("users", users);
+			req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+		} catch (DatabaseException e) {
+			throw new ServletException(e);
+		}
+		
+	}
+	
 }
