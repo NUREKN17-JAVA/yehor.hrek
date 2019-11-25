e6b7ea7af3de35fb0c8544aae546221418a348dd
EgorGrek <gromer349@gmail.com>
2019-11-25 13:15:50 +0200

    add TickerBehavior

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/agent/SearchAgent.java
@@ -4,14 +4,18 @@ import java.util.Collection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
+import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import ua.nure.kn.hrek.usermanagement.db.DaoFactory;
 import ua.nure.kn.hrek.usermanagement.db.DatabaseException;
 
 public class SearchAgent extends Agent {
-
+	
+	private AID[] aids;
+	
 	@Override
 	protected void setup() {
 		super.setup();
@@ -25,13 +29,38 @@ public class SearchAgent extends Agent {
 		description.addServices(serviceDescription);
 		try {
 			DFService.register(this, description);
+		}catch (FIPAException e) {
+			e.printStackTrace();
 		}
+		
 		addBehaviour(new RequestServer());
+		
+		addBehaviour(new TickerBehaviour(this, 60000) {
+			
+			@Override
+			protected void onTick() {
+				DFAgentDescription agentDescription = new DFAgentDescription();
+				ServiceDescription serviceDescription = new ServiceDescription();
+				serviceDescription.setType("searching");
+				agentDescription.addServices(serviceDescription);
+				try {
+					DFAgentDescription[] descriptions = DFService.search(myAgent, agentDescription);
+					aids = new AID[descriptions.length];
+				}catch (FIPAException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	@Override
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated.");
+		try {
+			DFService.deregister(this);
+		}catch (FIPAException e) {
+			e.printStackTrace();
+		}
 		super.takeDown();
 	}
 	
@@ -41,7 +70,7 @@ public class SearchAgent extends Agent {
 			if(users.size() > 0) {
 				showUsers(users);
 			}else {
-				addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+				addBehaviour(new SearchRequestBehaviour(aids, firstName, lastName));
 			}
 		}catch(DatabaseException e){
 			throw new SearchException(e);
