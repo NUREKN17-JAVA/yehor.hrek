e14988493556889fcb554536f80803a381ddf190
EgorGrek <gromer349@gmail.com>
2019-11-26 22:13:57 +0200

    minor improvements

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/EditServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/EditServlet.java
@@ -51,12 +51,12 @@ public class EditServlet extends HttpServlet {
 		;
 	}
 
-	private void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/edit.jsp").forward(req, resp);
 		
 	}
 
-	private void processUser(User user) throws DatabaseException {
+	protected void processUser(User user) throws DatabaseException {
 		DaoFactory.getInstance().getUserDao().update(user);
 
 	}
