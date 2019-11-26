8da5fe4d1d4902753cff7ae9f7c64d6f3e6683a0
EgorGrek <gromer349@gmail.com>
2019-11-26 19:14:53 +0200

    bug fix

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ua.nure.kn.hrek.usermanagement.User;
 import ua.nure.kn.hrek.usermanagement.db.DaoFactory;
 import ua.nure.kn.hrek.usermanagement.db.DatabaseException;
 
@@ -16,8 +17,49 @@ public class BrowseServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		if(req.getParameter("addButton") != null) {
+			add(req, resp);
+		}else if(req.getParameter("editButton") != null) {
+			edit(req, resp);
+		}else if(req.getParameter("deleteButton") != null) {
+			delete(req, resp);
+		}else if(req.getParameter("detailsButton") != null){
+			details(req, resp);
+		}else {
+			browse(req, resp);
+		}
+	}
+
+	private void details(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		browse(req, resp);
+		
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String idStr = req.getParameter("id");
+		if(idStr == null || idStr.trim().length() == 0) {
+			req.setAttribute("error", "You must select a user");
+			req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+			return;
+		}
+		try {
+			User user = DaoFactory.getInstance().getUserDao().find(new Long(idStr));
+			req.getSession().setAttribute("user", user);
+		}catch (Exception e) {
+			req.setAttribute("error", "ERROR:" + e.toString());
+			req.getRequestDispatcher("/browse.jps").forward(req, resp);
+			return;
+		}
+		req.getRequestDispatcher("/edit").forward(req, resp);;
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		
 	}
 
