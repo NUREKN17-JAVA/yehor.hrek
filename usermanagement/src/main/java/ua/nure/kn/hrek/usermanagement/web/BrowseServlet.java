e1ee2c96d5038b0ebcc866219f7c673ce4f4729a
EgorGrek <gromer349@gmail.com>
2019-11-26 22:13:39 +0200

    impl method add

--- usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/hrek/usermanagement/web/BrowseServlet.java
@@ -59,8 +59,7 @@ public class BrowseServlet extends HttpServlet {
 	}
 
 	private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		req.getRequestDispatcher("/add").forward(req, resp);
 	}
 
 	private void browse(HttpServletRequest req, HttpServletResponse resp) 
